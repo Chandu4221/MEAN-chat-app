8d85b43bdeaa6bfe0524a5acc584dbeed84dac39
chandra <chndraja@gmail.com>
2020-10-15 10:29:26 +0530

    root routing added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  {path:"",redirectTo:"/login",pathMatch:"full"}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
